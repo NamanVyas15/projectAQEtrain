2614ce63d64d79a9ea5a53ca2855f6baeb0bc23d
NamanVyas15 <namanvyas4017@gmail.com>
2024-06-26 16:48:52 +0530

    commit

---
+++ src/main/java/com/example/projectAQE/demo/AqeTrainingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectAQE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqeTrainingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqeTrainingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectAQE/demo/AqeTrainingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectAQE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqeTrainingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
