ca4127291ad7d392b3447df269b954cb0da9b524
NamanVyas15 <namanvyas4017@gmail.com>
2024-06-27 08:47:54 +0530

    updated

--- src/main/java/com/example/projectAQE/demo/AqeTrainingProjectApplication.java
+++ src/main/java/com/example/projectAQE/demo/AqeTrainingProjectApplication.java
@@ -2,10 +2,15 @@ package com.example.projectAQE.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AqeTrainingProjectApplication {
 
+	@GetMapping("/message")
+	public String message(){
+		return "hello world";
+}
 	public static void main(String[] args) {
 		SpringApplication.run(AqeTrainingProjectApplication.class, args);
 	}
