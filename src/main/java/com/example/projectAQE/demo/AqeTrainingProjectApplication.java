53640970d3143a382f801632c54df67d3f9cfd8d
NamanVyas15 <namanvyas4017@gmail.com>
2024-06-28 12:14:19 +0530

    updated

--- src/main/java/com/example/projectAQE/demo/AqeTrainingProjectApplication.java
+++ src/main/java/com/example/projectAQE/demo/AqeTrainingProjectApplication.java
@@ -2,15 +2,31 @@ package com.example.projectAQE.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class AqeTrainingProjectApplication {
 
-	@GetMapping("/message")
-	public String message(){
-		return "hello world";
-}
+
+	@GetMapping("/hello")
+	public String sayhello(){
+		return "helloworld"; //file name
+
+	}
+	@GetMapping("/h")
+	public String say(){
+		return "jk"; //file name
+
+	}
+	@GetMapping("/z")
+	public String aon(){
+		return "hello there!!"; //file name
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AqeTrainingProjectApplication.class, args);
 	}
